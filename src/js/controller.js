96ed04a66074ecf64cad12be358d9e9e9aa91b88
mahtaJannatifar <mahj7596@gmail.com>
2022-07-23 18:44:51 -0700

    add new welcome message

--- src/js/controller.js
+++ src/js/controller.js
@@ -154,6 +154,9 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(err.message);
   }
 };
+const newFeature = function () {
+  console.log('welcome to forkify');
+};
 //subscriber
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -163,5 +166,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
